cbd7eb6b516cc3521102cc12edb7f42b4feb88cf
MichaelX <tomichael23@gmail.com>
2019-06-10 16:44:55 -0700

    added learning curve algorithm

---
+++ algorithm-evaluation/linearRegCostFunction.m
@@ -0,0 +1,45 @@
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear
+%regression with multiple variables
+%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the
+%   cost of using theta as the parameter for linear regression to fit the
+%   data points in X and y. Returns the cost in J and the gradient in grad
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost and gradient of regularized linear
+%               regression for a particular choice of theta.
+%
+%               You should set J to the cost and grad to the gradient.
+%
+
+hypothesis = X*theta;
+var = (hypothesis - y) .^ 2;
+total = sum(var);
+J = (1/(2*m))*total;
+
+theta_reg = theta([2:size(theta)],:);
+Reg = (lambda/(2*m)) * sum(theta_reg .^ 2);
+J = J + Reg;
+
+% size(theta)
+weight = (lambda/m) * ones(size(theta,1)-1,1);
+% size(weight)
+weight = [zeros(1,columns(weight)); weight];
+% size(weight)
+scale = weight .* theta;
+cost = hypothesis - y;
+grad = grad + ((1/m) * X' * cost) + scale;
+
+
+% =========================================================================
+
+grad = grad(:);
+
+end
