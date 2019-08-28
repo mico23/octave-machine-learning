5a3720ab46100d6143061cef3a632bd7734e549e
mico23 <en.xie.michael@gmail.com>
2019-08-27 23:54:13 -0700

    created support vector machine project - spam email filter

---
+++ support-vector-machine/dataset3Params.m
@@ -0,0 +1,56 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.1;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example,
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using
+%        mean(double(predictions ~= yval))
+%
+
+% initialize error index
+error_index = 100;
+
+%initialize parameters
+parameters = [0.01 0.03 0.1 0.3 1 3 10 30];
+
+% use different C and sigma to train X and y
+% use the theta obtained from svmTrain to
+% compute the error of the validation set
+% loop through all the combiniations
+% compare the current error value with the previous one
+% if the smaller error is found, store the current combination
+
+% for i = 1:size(parameters(:))
+%  C_guess = parameters(i);
+%  for j = 1:size(parameters(:))
+%    sigma_guess = parameters(j);
+%    model= svmTrain(X, y, C_guess, @(x1, x2) gaussianKernel(x1, x2, sigma_guess));
+%    predictions = svmPredict(model, Xval);
+%    error = mean(double(predictions ~= yval));
+%    if error <= error_index
+%      C = C_guess;
+%      sigma = sigma_guess;
+%      error_index = error;
+%    end
+%  end
+%end
+
+% =========================================================================
+
+end
